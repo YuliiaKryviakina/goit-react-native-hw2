90179b1149c1e2c1e5b6ac40672c1983dd30a2ef
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-08-01 23:41:10 +0300

    add task 1.1

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import RegistrationScreen from "./Screens/AuthPages/RegistrationScreen/RegistrationScreen";
+import LoginScreen from "./Screens/AuthPages/LoginScreen/LoginScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <RegistrationScreen />
+      {/* <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
