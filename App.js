3c0fcf6dcb406a263e43d709a85e7320b04d42f5
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-08-02 22:29:36 +0300

    hw2

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 // import RegistrationScreen from "./Screens/AuthPages/RegistrationScreen/RegistrationScreen";
-// import LoginScreen from "./Screens/AuthPages/LoginScreen/LoginScreen";
+import LoginScreen from "./Screens/AuthPages/LoginScreen/LoginScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
       <StatusBar style="auto" />
     </View>
   );
