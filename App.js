fbbc536855b4fee6034d5097c1f05f42bb25352e
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-08-02 09:52:46 +0300

    add bg image

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import RegistrationScreen from "./Screens/AuthPages/RegistrationScreen/RegistrationScreen";
-import LoginScreen from "./Screens/AuthPages/LoginScreen/LoginScreen";
+// import RegistrationScreen from "./Screens/AuthPages/RegistrationScreen/RegistrationScreen";
+// import LoginScreen from "./Screens/AuthPages/LoginScreen/LoginScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      <RegistrationScreen />
-      {/* <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" /> */}
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
